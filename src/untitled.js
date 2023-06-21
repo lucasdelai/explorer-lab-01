bf8ff6bb12f0ae2bc9bf99634e18945e7da26db0
Lucas Delai <lucas-delai@hotmail.com>
2023-06-21 11:25:15 -0300

    project finished

---
+++ src/untitled.js
@@ -0,0 +1,14 @@
+// visa ^4\d{0,15}
+// inicia com 4 seguido de mais 15 dígitos
+4321234231234344
+
+// Master (5[1-5]\d{0,2}|^22[2-9]\d|^2[3-7]\d{0,2})\d{0,12}
+// Inicia com 5, seguido de um dígito entre 1 e 5, seguido de mais 2 dígitos
+// ou
+// inicia com 22, seguido de um dígito entre 2 e 9, seguido de mais 1 dígito
+// ou
+// Inicia com 2, seguido de um dígito entre 3 e 7, seguido de mais 2 dígitos
+// seguido de mais 12 dígitos
+5353535353535353
+2323232323232323
+2234412412412414
